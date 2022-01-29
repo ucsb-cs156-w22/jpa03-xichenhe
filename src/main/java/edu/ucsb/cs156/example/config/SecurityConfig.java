3966a1193c50c551119dbb80eb61e8c1939b1ecc
xichenhe <xichenhe@ucsb.edu>
2022-01-28 21:52:13 -0800

    changed

--- src/main/java/edu/ucsb/cs156/example/config/SecurityConfig.java
+++ src/main/java/edu/ucsb/cs156/example/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
